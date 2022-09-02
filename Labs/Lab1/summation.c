5bdfd32908811da60a78d2f55d2b733dee53b7c5
Robert Krency <bobkrency@gmail.com>
2022-09-01 21:46:02 -0400

    Lab1 - Initial Version, needs testing for upper limit on stack  overflow prevention

---
+++ Labs/Lab1/summation.c
@@ -0,0 +1,44 @@
+/*
+ *  @author Robert Krency, kre1188@pennwest.edu
+ *  @date 8/26/22
+ * 
+ *  Summation.c - A simple recursive program for summing all positive integers
+ *    up to and including the input integer from a user.
+ *  
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+long long summation (long long i) {
+	return (i == 0) ? 0 : i + summation(i - 1);
+}
+
+int main()
+{
+	long long number, sum=0;
+
+	printf("Summation Calculator\n");
+	printf("Please enter a nonnegative integer (0 - 2147483647): ");
+
+	// Credit for input error handling loop:
+	//		https://stackoverflow.com/a/26583890
+	// Reason for usage:
+	//		Scanf requires many error checking edge cases that cannot all
+	//		be handled elegantly, or even guaranteed to be found.
+	char *p, s[100];
+	while (fgets(s, sizeof(s), stdin)) {
+        number = strtoll(s, &p, 10);
+        if (p == s || *p != '\n'|| number < 0 || number > 2147483647) {
+			printf("Invalid entry.\n");
+            printf("Please enter a nonnegative integer (0 - 2147483647): ");
+        } else break;
+    }
+
+	printf("Calculating summation for %lld.\n", number);
+	
+	sum = summation(number);
+	
+	printf( "The summation of %lld is %lld.\n", number, sum);
+	return 0;
+}
